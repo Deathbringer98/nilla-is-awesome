b0399ec7db8ce6b416bfd5cd16e76b875c5e9d0d
Matt <73149745+Deathbringer98@users.noreply.github.com>
2024-07-26 11:04:27 -0230

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 window.onload = function() {
     var container = document.getElementById('stream-container');
     var iframe = document.createElement('iframe');
-    iframe.src = 'https://kick.com/nillavanilla';
+    iframe.src = 'https://player.kick.com/nillavanilla';
     iframe.width = '100%';
     iframe.height = '100%';
     iframe.frameBorder = '0';
+    iframe.scrolling = 'no';
     iframe.allowFullscreen = true;
     container.appendChild(iframe);
 };
